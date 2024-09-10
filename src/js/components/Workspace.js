5d4936cee7e91ec6d0af70314f3ec5978c5eaafb
kos4 <kos4.kks@gmail.com>
2024-09-10 10:33:01 +0500

    Сделал основу - чат

--- src/js/components/Workspace.js
+++ src/js/components/Workspace.js
@@ -5,7 +5,94 @@ export default class Workspace {
   }
 
   init() {
+    this.subscribeOnEvents();
+  }
+
+  subscribeOnEvents() {
+    this.websocket = new WebSocket('ws://localhost:8082');
+
+    this.websocket.addEventListener('open', () => {
+      this.renderChat();
+      this.enterSendForm();
+    });
+
+    this.websocket.addEventListener('message', (e) => {
+      const data = JSON.parse(e.data);
+
+      if (data.status === 'connect') {
+        this.user.connectionId = data.id;
+        this.websocket.send(JSON.stringify({
+          type: 'getData', userId: this.user.id,
+        }));
+      } else {
+        if (data.userId === this.user.id) {
+          if (Object.hasOwn(data, 'chat')) {
+            if (data.id === this.user.connectionId) {
+              data.chat.forEach(item => {
+                this.renderMessage(item);
+              });
+            }
+          }else {
+            this.renderMessage(data);
+          }
+        }
+      }
+    });
+  }
+
+  enterSendForm() {
+    document.getElementById("message").addEventListener("keypress", e => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        this.submitForm(e.currentTarget.closest('form'));
+      }
+    });
+  }
+
+  submitForm(form) {
+    const message = form.message.value;
+
+    if (!message) return;
+
+    this.sendMessage(message);
+    form.message.value = '';
+  }
+
+  sendMessage(message) {
+    const json = JSON.stringify({
+      type: 'send', message, date: Date.now(), userId: this.user.id,
+    });
+    this.websocket.send(json);
+  }
+
+  renderMessage(data) {
+    const chat = document.querySelector('.content');
+    const message = this.markupMessage(data);
+    chat.insertAdjacentHTML('beforeend', message);
+    chat.scrollTop = chat.scrollHeight;
+  }
+
+  renderChat() {
     this.app.innerHTML = this.markup();
+  }
+
+  markupMessage(data) {
+    return `
+      <div class="message__container">
+        <div class="message__container-interlocutor">
+          ${data.message}
+        </div>
+        <div class="message__header">
+          ${new Date(data.date).toLocaleDateString(undefined, {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+          })}
+        </div>
+      </div>
+    `;
   }
 
   markup() {
@@ -32,7 +119,7 @@ export default class Workspace {
       <div class="footer">
         <div class="footer__files"></div>
         <div class="footer__message">
-          <form class="form" action="">
+          <form class="form form__message" action="">
             <div class="form__group">
               <label for="message"></label>
               <textarea class="form__textarea" id="message" name="message" placeholder="Введите сообщение..."></textarea>
