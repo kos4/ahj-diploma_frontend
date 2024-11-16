9544906bddac74036b2f25854c1225d5fcde4ee1
kos4 <kos4.kks@gmail.com>
2024-11-16 13:10:14 +0500

    Сделал определение текущий координат

---
+++ src/js/components/Popup/Popup.js
@@ -0,0 +1,50 @@
+import './popup.css';
+
+export default class Popup {
+  constructor() {
+    this.container = document.querySelector('body');
+  }
+
+  render(data) {
+    const markup = this.markup(data);
+
+    this.removePopup();
+    this.container.insertAdjacentHTML('beforeend', markup);
+
+    const elPopup = this.container.querySelector('.popup');
+    const close = elPopup.querySelector('.popup__close');
+    const popupWindow = elPopup.querySelector('.popup__window');
+
+    popupWindow.addEventListener('click', event => event.stopPropagation());
+    close.addEventListener('click', this.closePopup.bind(this));
+    elPopup.addEventListener('click', this.closePopup.bind(this));
+  }
+
+  closePopup() {
+    this.removePopup();
+  }
+
+  removePopup() {
+    const items = this.container.querySelectorAll('.popup');
+
+    if (items.length) {
+      items.forEach((item) => {
+        item.remove();
+      });
+    }
+  }
+
+  markup(data) {
+    const title = data.title ? data.title : 'Сообщение';
+
+    return `
+      <div class="popup">
+        <div class="popup__window">
+          <div class="popup__close">x</div>
+          <div class="popup__title">${title}</div>
+          <div class="popup__body">${data.body}</div>
+        </div>
+      </div>
+    `;
+  }
+}
--- src/js/components/workspace/handlers/onSubmitMessage.js
+++ src/js/components/workspace/handlers/onSubmitMessage.js
@@ -1,6 +1,7 @@
 import updateFileList from "./updateFileList";
 import Entity from "../../../classes/Entity";
 import sendFiles from "./sendFiles";
+import Popup from "../../Popup/Popup";
 
 const entity = new Entity();
 
@@ -8,6 +9,7 @@ export default function onSubmitMessage(form, webSoc, user) {
   const message = form.message.value;
   const fileInput = document.querySelector('.form__input-files');
   const files = fileInput.files;
+  const popup = new Popup();
 
   if (!message && !files.length) return;
 
@@ -20,11 +22,45 @@ export default function onSubmitMessage(form, webSoc, user) {
 
     entity.sendFiles(formData, user.id, sendFiles.bind(null, webSoc, json));
     fileInput.value = '';
+    form.message.value = '';
     updateFileList(fileInput);
-  } else {
-    json.message = message;
-    webSoc.sendMessage(json);
+
+    return;
   }
 
+  if (message === '@location') {
+    let sendMessage = '';
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        data => {
+          sendMessage = `Ваша локация: ${data.coords.latitude} широты, ${data.coords.longitude} долготы.`;
+          json.message = sendMessage;
+          webSoc.sendMessage(json);
+        },
+        error => {
+          popup.render({
+            title: `Ошибка. Код: ${error.code}`,
+            body: error.message,
+          });
+          console.log(error);
+        },
+        {
+          enableHighAccuracy: true,
+        }
+      );
+    } else {
+      popup.render({
+        body: 'Ваш браузер не поддерживает определение Геолокации.',
+      });
+    }
+
+    form.message.value = sendMessage;
+
+    return;
+  }
+
+  json.message = message;
+  webSoc.sendMessage(json);
   form.message.value = '';
 }
